d66eb32914b8f7cccc08834f26b24d9e41cbd320
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2022-11-09 09:26:42 +0100

    Update WagonCarte.java
    Création des constructeur et des getteur et setteur

--- WagonCarte.java
+++ WagonCarte.java
@@ -1,3 +1,42 @@
 public class WagonCarte extends Carte{
     private String couleurWagon;
+    private boolean joker;
+
+    public WagonCarte(){
+        this.couleurWagon = new String ("red");
+        this.joker = false;
+    }
+
+    public WagonCarte(String couleur){
+        this.couleurWagon = new String(couleur);
+        this.joker = false;
+    }
+
+    public WagonCarte(String couleur, boolean joker){
+        this.couleurWagon = new String(couleur);
+        this.joker = joker;
+    }
+
+    public WagonCarte(WagonCarte other){
+        this.couleurWagon = new String(other.getCouleurWagon());
+        this.joker = other.isJoker();
+    }
+
+
+    public String getCouleurWagon() {
+        return this.couleurWagon;
+    }
+
+    public void setCouleurWagon(String couleurWagon) {
+        this.couleurWagon = couleurWagon;
+    }
+
+    public boolean isJoker() {
+        return this.joker;
+    }
+
+    public void setJoker(boolean joker) {
+        this.joker = joker;
+    }
+
 }
