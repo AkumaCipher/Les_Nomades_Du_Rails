b0b1332d414b5dd7b60fc89619398325f693bec1
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2022-11-09 08:55:10 +0100

    Update Route.java
    Ajout des getteur et setteur

--- Route.java
+++ Route.java
@@ -2,4 +2,29 @@ public class Route {
     private int taille;
     private Ville[] destination = new Ville[2];
     private String couleur;
+
+    public int getTaille() {
+        return this.taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public Ville[] getDestination() {
+        return this.destination;
+    }
+
+    public void setDestination(Ville[] destination) {
+        this.destination = destination;
+    }
+
+    public String getCouleur() {
+        return this.couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
 }
