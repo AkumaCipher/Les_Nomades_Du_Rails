a3b5ed817705e37371ff616b674c42c18f25025b
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2022-11-09 09:48:26 +0100

    Update PaquetCarte.java
    Ajout d'un constructeur, getteur, setter et l'ajout d'une carte

--- PaquetCarte.java
+++ PaquetCarte.java
@@ -2,4 +2,21 @@ import java.util.ArrayList;
 
 public class PaquetCarte {
     private ArrayList<Carte> paquet = new ArrayList<Carte>();
+
+    public PaquetCarte(){
+
+    }
+
+
+    public ArrayList<Carte> getPaquet() {
+        return this.paquet;
+    }
+
+    public void setPaquet(ArrayList<Carte> paquet) {
+        this.paquet = paquet;
+    }
+
+    public void addCarte(Carte carte){
+        paquet.add(carte);
+    }
 }
