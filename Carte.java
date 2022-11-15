893b16adc2a6c896ec41210bebac2a250bdfe094
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2022-11-15 15:52:30 +0100

    Update Carte.java
    Ajout de la variable commun type avce getter et setter

--- Carte.java
+++ Carte.java
@@ -1,3 +1,11 @@
 public abstract class Carte {
+    protected String type;
+    
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type){
+        this.type = type;
+    }
 }
